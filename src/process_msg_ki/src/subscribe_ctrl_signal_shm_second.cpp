e4d4ea2105d6a9d929800bfe731bb834279315c2
lenardxu <67544149+lenardxu@users.noreply.github.com>
2022-03-07 23:48:33 +0100

    Add the src file of subscribing control signal and saving it into shared memory

---
+++ src/process_msg_ki/src/subscribe_ctrl_signal_shm_second.cpp
@@ -0,0 +1,69 @@
+//
+// Created by Lenovo on 2/16/2022.
+//
+
+#include "ros/ros.h"
+#include <geometry_msgs/PoseStamped.h>
+#include <boost/interprocess/windows_shared_memory.hpp>
+#include <boost/interprocess/mapped_region.hpp>
+
+using namespace boost::interprocess;
+
+
+/*
+ * \brief callback method should be called when receiving the full 3d pose, which then publishes the dummy control
+ * signal, which consists of the corrected positions
+ * \param ps3d shared Pointer to the incoming full 3d pose
+ * \param m_region reference to the mapped region in this process
+ * \return None
+ */
+void positionCallback(const geometry_msgs::PoseStamped::ConstPtr& ps3d, mapped_region& m_region){
+    ROS_INFO("Received position: X=%.2f, Y=%.2f, Z=%.2f; orientation quaternion: x=%.2f, y=%.2f, z=%.2f, w=%.2f",
+             ps3d->pose.position.x, ps3d->pose.position.y, ps3d->pose.position.z,
+             ps3d->pose.orientation.x, ps3d->pose.orientation.y, ps3d->pose.orientation.z, ps3d->pose.orientation.w);
+    // define and initiate rkorrs array
+    double rkorrs[3]{0.0, 0.0, 0.0};
+    // assign the positions to rkorrs
+    rkorrs[0] = ps3d->pose.position.x;
+    rkorrs[1] = ps3d->pose.position.y;
+    rkorrs[2] = ps3d->pose.position.z;
+    // update the mapped region of this process with rkorrs (corrected positions)
+    std::memcpy(m_region.get_address(), rkorrs, 3*sizeof(double));
+}
+
+/*
+ * \brief Dummy controller for receiving pose and sending control signal - corrected positions
+ *
+ * \param argc An integer argument count of the command line arguments
+ * \param argv An argument vector of the command line arguments
+ * \return Status of the main program
+ */
+int main(int argc, char **argv){
+    //Create a native windows shared memory object.
+    //Note: No need to explicitly remove the share memory segments due to use of native windows shared memory mechanism
+    windows_shared_memory shm (create_only, "SecondSharedMemory", read_write, 1024);
+    //Map the whole shared memory in this process
+    mapped_region region(shm, read_write);
+
+    // initialize ROS with specifying the name of node constructed in this process
+    ros::init(argc, argv, "subscribe_ctrl_signal_node");
+    // create a handle to this process's node
+    ros::NodeHandle n;
+    // TODO there may be problem in this std::bind w.r.t parameters, please first run and check it
+    // create a function obj based on positionCallback, but with its arguments extended with mapped region
+    auto boundPositionCallback = std::bind(positionCallback, std::placeholders::_1, std::cref(region));
+    // TODO to set the queue size reasonably
+    // let the node subscribe the ros msg - 3d pose without explicit rotation - over topic "control_signal"
+    ros::Subscriber delta_positions_sub = n.subscribe<geometry_msgs::PoseStamped>("control_signal",
+                                                                  1000,
+                                                                  boundPositionCallback);
+    /**
+     * ros::spin() will enter a loop, pumping callbacks.  With this version, all
+     * callbacks will be called from within this thread (the main one).  ros::spin()
+     * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
+     */
+    // Use spin other spinOnce in a loop due to the fact that we only need to respond to the callback
+    ros::spin();
+    //ros::shutdown();
+    return 0;
+}
